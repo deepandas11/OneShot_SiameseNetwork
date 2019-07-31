6eec1df349b083208b4309cc31b7d416824224d4
deepandas11 <deepandas11@gmail.com>
2019-07-31 13:57:58 +0530

    Learning rate scheduler
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- utils.py
+++ utils.py
@@ -1,9 +1,4 @@
 
+import tensorflow as tf
 
 
-def gt_label_gen(a,b):
-    """
-    :params a,b: Two integers/Class categories.
-    :return 0/1: If they belong to same class, return 1, else 0
-    """
-    return int(not bool(a-b))
