ee2a519b2e05f319acf0a85d2358dc71f92ce594
deepandas11 <deepandas11@gmail.com>
2019-08-01 19:57:32 +0530

    Main script to train model
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from model import siamese_architecture
 from dataloader import DataGenerator
+from train import *
 
 import argparse
+from datetime import datetime
+import keras
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -11,15 +14,29 @@ parser.add_argument('--b', default=32, type=int, help='Batch Size')
 
 
 def main(args):
-       
+
+    print("Starting")
+    callbacks_list = list()
+
+    
+    logdir = 'logs/scalars/'+datetime.now().strftime('%Y%m%d-%H%M%S')
+    # Keras Tensorboard callback
+
+    tb_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    callbacks_list.append(tb_callback)
+    print(callbacks_list)
+
     train_loader = DataGenerator(mode='train')
     val_loader = DataGenerator(mode='val')
 
+    print('loaded data generators')
     model = siamese_architecture()
 
-    model.fit_generator(generator=train_loader,
-                        validation_data=val_loader,
-                        epochs=args.n_epochs)
+    print('training')
+    train(n_epochs=args.n_epochs, lr=args.lr,
+          model=model, train_gen=train_loader,
+          val_gen=val_loader, callbacks_list=callbacks_list)
+
 
 if __name__=='__main__':
     args = parser.parse_args()
