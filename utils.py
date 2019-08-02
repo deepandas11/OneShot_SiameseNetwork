95df602bece2965846ad2b9c5c626954b8c13103
deepandas11 <deepandas11@gmail.com>
2019-08-02 19:32:54 +0530

    LR scheduler added
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-
 import tensorflow as tf
+import keras.backend as K
 
+
+def lr_scheduler(epoch, model):
+    if (epoch+1)%2 == 0:
+        K.set_value(model.optimizer.lr, K.get_value(model.optimizer.lr)*0.99)
 
